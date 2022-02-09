f94aa3f74b58dc15cd7292538a4631e3bf199386
George BV <egor.workroom@gmail.com>
2022-02-09 12:09:04 +0300

    fixed: not track of changes in status of mirror task

--- clickup/sync_mirror_task.go
+++ clickup/sync_mirror_task.go
@@ -307,13 +307,6 @@ func (s *mirrorTaskSyncer) applyChangesToMirrorTask(ctx context.Context, mirror 
 
 	// TODO: description change
 
-	// status
-	if oldTask != nil && origTask.StatusName != task.StatusName {
-		// TODO: изменился статус у задачи
-		fmt.Fprintf(commentText, "- changed task status name from %q to %q\n", origTask.StatusName, task.StatusName)
-		needToSendComment = true
-	}
-
 	// estimate
 	totalEstimate := task.TotalEstimate()
 	if origTask.TimeEstimateMs != nil && totalEstimate == 0 {
